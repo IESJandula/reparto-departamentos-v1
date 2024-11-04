22446b8326877cf6e50dbb05ffacf096684ee521
David FF <95282706+David198833@users.noreply.github.com>
2024-11-04 16:35:00 +0100

    Primer commit

---
+++ src/main/java/com/example/Reparto_Departamentos/RepartoDepartamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reparto_Departamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepartoDepartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepartoDepartamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reparto_Departamentos/RepartoDepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reparto_Departamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepartoDepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
